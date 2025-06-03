e6a926a08470e7ee8833e3c93ca3c8497202fd78
chrmqgozj <danmuginima@naver.com>
2025-06-03 18:58:40 +0900

    Edit cpp

--- tcp-block.cpp
+++ tcp-block.cpp
@@ -11,6 +11,7 @@ void usage() {
 	printf("sample : tcp-block wlan0 \"Host: test.gilgil.net\"\n");
 }
 
+// checksum calculation (ip, tcp)
 unsigned short checksum(unsigned short *buffer, int size){
 	unsigned long cksum=0;
 	while(size >1) {
@@ -25,6 +26,7 @@ unsigned short checksum(unsigned short *buffer, int size){
 	return (unsigned short)(~cksum);
 }
 
+// send packet to server (forward)
 void send_forward_packet(pcap_t* pcap, struct libnet_ethernet_hdr *eth_hdr, struct libnet_ipv4_hdr *ipv4_hdr, struct libnet_tcp_hdr *tcp_hdr, int data_len) {
 	int eth_hl, ipv4_hl, tcp_hl;
 	eth_hl = LIBNET_ETH_H;
@@ -40,12 +42,14 @@ void send_forward_packet(pcap_t* pcap, struct libnet_ethernet_hdr *eth_hdr, stru
 	struct libnet_ipv4_hdr* new_ipv4 = (struct libnet_ipv4_hdr*)(packet + eth_hl);
 	memcpy(new_ipv4, ipv4_hdr, ipv4_hl);
 
+	// tcp rst packet
 	struct libnet_tcp_hdr* new_tcp = (struct libnet_tcp_hdr*)(packet + eth_hl + ipv4_hl);
 	memcpy(new_tcp, tcp_hdr, tcp_hl);
 	new_tcp->th_flags = TH_ACK | TH_RST;
 	new_tcp->th_seq = htonl(ntohl(tcp_hdr->th_seq) + data_len);
 	new_tcp->th_sum = 0;
 
+	// calculate tcp checksum
 	u_char pseudo_hdr[12 + tcp_hl];
 	memcpy(pseudo_hdr, &new_ipv4->ip_src.s_addr, 4);
 	memcpy(pseudo_hdr + 4, &new_ipv4->ip_dst.s_addr, 4);
@@ -61,6 +65,7 @@ void send_forward_packet(pcap_t* pcap, struct libnet_ethernet_hdr *eth_hdr, stru
 	return;
 }
 
+// send packet to client (backward)
 void send_backward_packet(pcap_t* pcap, struct libnet_ethernet_hdr *eth_hdr, struct libnet_ipv4_hdr *ipv4_hdr, struct libnet_tcp_hdr *tcp_hdr, int data_len) {
 	const char* warning = "HTTP/1.0 302 Redirect\r\nLocation: http://warning.or.kr\r\n\r\n";
 	int payload_len = strlen(warning);
@@ -69,24 +74,29 @@ void send_backward_packet(pcap_t* pcap, struct libnet_ethernet_hdr *eth_hdr, str
         ipv4_hl = (ipv4_hdr -> ip_hl) * 4;
         tcp_hl = (tcp_hdr -> th_off) * 4;
 
+	// raw socket
 	int sd = socket(AF_INET, SOCK_RAW, IPPROTO_RAW);
 	if (sd == -1) {
 		fprintf(stderr, "socket open error\n");
 		return;
 	}
-	int on = 1;
+
+	char on = 1;
 	setsockopt(sd, IPPROTO_IP, IP_HDRINCL, (char *)&on, sizeof(on));	
 
 	u_char packet[ipv4_hl + tcp_hl + payload_len];
 	memset(packet, 0, sizeof(packet));
 
+	// exchange server <-> client
 	struct libnet_ipv4_hdr* new_ipv4 = (struct libnet_ipv4_hdr*)packet;
 	memcpy(new_ipv4, ipv4_hdr, ipv4_hl);
 	new_ipv4->ip_src = ipv4_hdr->ip_dst;
 	new_ipv4->ip_dst = ipv4_hdr->ip_src;
 	new_ipv4->ip_len = htons(ipv4_hl + tcp_hl + payload_len);
+	new_ipv4->ip_sum = 0;
 	new_ipv4->ip_sum = checksum((unsigned short*)new_ipv4, ipv4_hl);
 
+	// exchange server <-> client / tcp fin packet
 	struct libnet_tcp_hdr* new_tcp = (struct libnet_tcp_hdr*)(packet + ipv4_hl);
 	memcpy(new_tcp, tcp_hdr, tcp_hl);
 	new_tcp->th_sport = tcp_hdr->th_dport;
@@ -96,11 +106,13 @@ void send_backward_packet(pcap_t* pcap, struct libnet_ethernet_hdr *eth_hdr, str
 	new_tcp->th_flags = TH_ACK | TH_FIN;
 	new_tcp->th_sum = 0;
 
+	// add warning payload
 	memcpy(packet + ipv4_hl + tcp_hl, warning, payload_len);
 
+	// calculate tcp checksum
 	u_char pseudo_hdr[12 + tcp_hl + payload_len];
-        memcpy(pseudo_hdr, &new_ipv4->ip_dst.s_addr, 4);
-        memcpy(pseudo_hdr + 4, &new_ipv4->ip_src.s_addr, 4);
+        memcpy(pseudo_hdr, &new_ipv4->ip_src.s_addr, 4);
+        memcpy(pseudo_hdr + 4, &new_ipv4->ip_dst.s_addr, 4);
         pseudo_hdr[8] = 0;
         memcpy(pseudo_hdr + 9, &new_ipv4->ip_p, 1);
         unsigned short tcp_len = htons(tcp_hl + payload_len);
@@ -110,12 +122,12 @@ void send_backward_packet(pcap_t* pcap, struct libnet_ethernet_hdr *eth_hdr, str
 
         new_tcp->th_sum = checksum((unsigned short*)pseudo_hdr, 12 + tcp_hl + payload_len);
 
+	// send through socket
 	struct sockaddr_in address;
 	address.sin_family = AF_INET;
 	address.sin_port = 0;
-	address.sin_addr.s_addr = ipv4_hdr->ip_dst.s_addr;
+	address.sin_addr.s_addr = new_ipv4->ip_src.s_addr;
 
-	printf("Sending packet\n");
 	int ret = sendto(sd, packet, sizeof(packet), 0x0, (struct sockaddr *)&address, sizeof(address));
 	if (ret < 0) {
 		fprintf(stderr, "Failed to send backward packet\n");
